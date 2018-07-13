a93429896b627dc150d019ca0f049b37873a511b
ObserverOfTime <chronobserver@disroot.org>
2018-07-13 20:07:27 +0300

    Add support for downloading

--- bin/transfer.js
+++ bin/transfer.js
@@ -11,7 +11,8 @@ const argv = require('minimist')(process.argv.slice(2), {
     'output',
     'decrypt',
     'password',
-    'file-name'
+    'file-name',
+    'download'
   ],
   alias: {
     'help': 'h',
@@ -21,15 +22,16 @@ const argv = require('minimist')(process.argv.slice(2), {
     'output': 'o',
     'decrypt': 'd',
     'password': 'p',
-    'copy-url': 'c',
+    'copy': 'c',
     'file-name': 'n',
-    'no-progress': 'N'
+    'no-progress': 'N',
+    'download': 'D'
   }
 });
 const cliProgress = require('cli-progress');
 const bar = new cliProgress.Bar({
   format: '{task}ing [{bar}] {percentage}%' +
-    ' | ETA: {eta_formatted} | {value}/{total}',
+  ' | ETA: {eta_formatted} | {value}/{total}',
   barsize: 30, fps: 2, etaBuffer: 4
 }, cliProgress.Presets.legacy);
 const clipboardy = require('clipboardy');
@@ -39,24 +41,30 @@ const help = `\
   CLI tool for easy file sharing with https://transfer.sh
 
   \x1b[4mUsage\x1b[0m
-    $ transfer <file> [OPTIONS]
+    $ transfer-js [FILE] [OPTIONS]
 
   \x1b[4mOptions\x1b[0m
     -m, --max-days [NUMBER]       Maximum number of days.
     -M, --max-downloads [NUMBER]  Maximum number of downloads.
+    -D, --download [URL]          Download file from URL.
     -n, --file-name [NAME]        Name to use for the upload.
     -p, --password [PASS]         Password used to encrypt the file.
     -d, --decrypt [FILE]          Decrypt the file (requires --password).
-    -o, --output [PATH]           Decrypted file output path.
-    -c, --copy-url                Copy the file URL to the clipboard.
+    -o, --output [PATH]           Decrypted/downloaded file output path.
+    -c, --copy                    Copy the file URL/path to the clipboard.
     -N, --no-progress             Don't show the progress bar.
     -v, --version                 Print the version and exit.
     -h, --help                    Print this help text and exit.
 
     \x1b[4mExamples\x1b[0m
-      $ transfer README.md -n README.tmp.md -m 1
-      $ transfer README.md -p p4ssw0rd
-      $ transfer -d README.md -p p4ssw0rd
+      Upload file for 1 day with a custom name:
+        $ transfer-js file.txt -n file.tmp -m 1
+      Encrypt and upload file:
+        $ transfer-js file.txt -p p4ssw0rd
+      Download file:
+        $ transfer-js -D https://transfer.sh/I2ea5/file.txt
+      Decrypt file:
+        $ transfer-js -d file.enc -p p4ssw0rd
 `;
 
 /**
@@ -74,20 +82,20 @@ function catchError(err) {
 }
 
 /**
- * URL handler
+ * File handler
  *
  * @function
- * @param {string} url - The URL of the file
+ * @param {string} file - The URL or path of the file
  */
-function gotUrl(url) {
+function gotFile(file) {
   if(!argv.N) {
     bar.update(bar.total);
     bar.stop();
   }
-  console.log(url);
+  console.log(file);
   if(argv.c) {
-    clipboardy.write(url).then(() => {
-      console.log(' \uD83D\uDCCB Link copied to clipboard');
+    clipboardy.write(file).then(() => {
+      console.log(' \uD83D\uDCCB Copied to the clipboard');
     }).catch(catchError);
   }
 }
@@ -129,7 +137,7 @@ if(argv.h) {
 }
 
 if (argv.v) {
-  console.log(pkg.name + ' v' + pkg.version);
+  console.log(`${pkg.name} v${pkg.version}`);
   process.exit(0);
 }
 
@@ -146,9 +154,15 @@ const httpOpts = {
 if(argv.m) httpOpts.headers['Max-Days'] = argv.m;
 if(argv.M) httpOpts.headers['Max-Downloads'] = argv.M;
 
-if(argv.d) { // Decrypt
-  if(!argv.p)
-    catchError(new Error('No password provided'));
+
+if(argv.D) { // Download
+  new Transfer(argv.D, opts, httpOpts)
+    .download(argv.o)
+    .progress(progressBar)
+    .then(gotFile)
+    .catch(catchError);
+} else if(argv.d) { // Decrypt
+  if(!argv.p) catchError(new Error('No password provided'));
   const ext = require('path').extname(argv.d);
   let output = (!ext) ? `${argv.d}-decrypted` :
     `${argv.d.replace(ext, '')}-decrypted${ext}`;
@@ -157,13 +171,13 @@ if(argv.d) { // Decrypt
     .decrypt(output)
     .then(() => console.log('Successfully decrypted in', output))
     .catch(catchError);
-} else { // Encrypt (if requested) and upload each file
+} else { // Upload
   if(argv._.length === 0) argv._.push('');
   for(const i in argv._) {
     new Transfer(argv._[i], opts, httpOpts)
       .upload()
       .progress(progressBar)
-      .then(gotUrl)
+      .then(gotFile)
       .catch(catchError);
   }
 }
